0a9655aec6e361e611b92509d7b058677b8b1a58
Miguel <miguemoscoso21@gmail.com>
2024-06-01 14:26:24 -0500

    Creación del proyecto

---
+++ src/main/java/com/tuligapro/TuLigaProApplication.java
@@ -0,0 +1,13 @@
+package com.tuligapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuLigaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuLigaProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuligapro/TuLigaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuligapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuLigaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
